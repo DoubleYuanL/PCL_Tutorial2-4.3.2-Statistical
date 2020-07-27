a50377765fe2f7bd66d8e7a86e6acf5e9b372f3e
DoubleYuanL <44334870+DoubleYuanL@users.noreply.github.com>
2020-07-27 15:28:42 +0800

    Add files via upload

---
+++ 4.3.2-statistical/main.cpp
@@ -0,0 +1,32 @@
+#include <pcl/io/pcd_io.h>
+#include <pcl/filters/statistical_outlier_removal.h>
+#include <iostream>
+#include <pcl/filters/filter.h>
+int
+main(int argc, char** argv)
+{
+	// Objects for storing the point clouds.
+	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+	pcl::PointCloud<pcl::PointXYZ>::Ptr filteredCloud(new pcl::PointCloud<pcl::PointXYZ>);
+
+	// Read a PCD file from disk.
+	if (pcl::io::loadPCDFile<pcl::PointXYZ>(argv[1], *cloud) != 0)
+	{
+		return -1;
+	}
+	/*
+	std::vector<int> mapping;
+	pcl::removeNaNFromPointCloud(*cloud, *cloud, mapping);
+	*/
+	// Filter object.
+	pcl::StatisticalOutlierRemoval<pcl::PointXYZ> filter;
+	filter.setInputCloud(cloud);
+	// Set number of neighbors to consider to 50.
+	filter.setMeanK(50);
+	// Set standard deviation multiplier to 1.
+	// Points with a distance larger than 1 standard deviation of the mean distance will be outliers.
+	filter.setStddevMulThresh(1.0);
+
+	filter.filter(*filteredCloud);
+	pcl::io::savePCDFileASCII("statistical_qutlier_remove.pcd", *filteredCloud);
+}
